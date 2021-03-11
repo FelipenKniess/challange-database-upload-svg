87d24b7dd56e28e4b5fe85d9954e1f52a28ba046
FelipenKniess <felipe.nkniess@gmail.com>
2021-03-11 01:01:01 -0300

    Realizado todos os testes, exceto o de uplod csv

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,49 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionRepository.find();
+  const balance = await transactionRepository.getBalance();
+
+  return response.json({ transactions, balance })
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const {title, value, type, category} = request.body;
+
+  const createTransactionService = new CreateTransactionService();
+
+  const newTransaction = await createTransactionService.execute({title, value, type, category});
+
+  return response.json(newTransaction);
 });
 
+
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactions = new DeleteTransactionService();
+
+  await deleteTransactions.execute(id);
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+  const importTransactionsService = new ImportTransactionsService();
+
+  const transactions = await importTransactionsService.execute();
+
+  return response.json( {transactions} );
 });
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,34 @@
+import { response } from 'express';
+import path from 'path';
+
 import Transaction from '../models/Transaction';
+import loadCSV from '../config/loadCsv';
+import CreateTransactionService from './CreateTransactionService';
+
+
 
 class ImportTransactionsService {
   async execute(): Promise<Transaction[]> {
-    // TODO
+    const csvFilePath = path.resolve(__dirname, '..', '__tests__', 'import_template.csv');
+
+    const loadCSVfile = await loadCSV(csvFilePath);
+
+    const transactionsService = new CreateTransactionService();
+
+    console.log(loadCSVfile);
+
+   const newTransactions = loadCSVfile.map(transaction => (
+     {
+       title: transaction[0],
+       type: transaction[1],
+       value: transaction[2],
+       category: transaction[3]
+     }
+   ));
+
+   console.log(newTransactions);
+
+    return newTransactions;
   }
 }
 
